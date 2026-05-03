80db5cc8308839e95c125253133d145b54852e2f
BobisBilly <bobisbilly.3115@gmail.com>
2026-05-04 01:47:50 +1200

    Update AssemblyInfo.cs
    Changed version to 1.1 and added BobisBilly to the author list

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using MelonLoader;
 using wilbupanel;
 
-[assembly: MelonInfo(typeof(wilbupanel.Main), "wilbupanel", "1.0", "wilburie")]
+[assembly: MelonInfo(typeof(wilbupanel.Main), "wilbupanel", "1.1", "wilburie, BobisBilly")]
 [assembly: MelonGame("The Sledding Corporation", null)]
 [assembly: MelonColor(0, 255, 114, 240)]
 
